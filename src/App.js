a254f9fd04e1bc244e00ea5b429fec1e3245ca6d
Neda Gholami <nedagholami1998@gmail.com>
2022-07-05 01:02:57 -0700

    add a few components to project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './Header';
+import Counter from './Counter';
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header>
+   <div>
+            <p className="counter">
+                Counter App
+            </p>
+        </div>
+   </header>
+  )
+}
+
+export default Header
